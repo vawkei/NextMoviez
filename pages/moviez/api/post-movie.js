058e4455e3deeecf62c389cd285502be38db7ebb
voke sokoh <vokes9810@gmail.com>
2023-04-02 18:28:16 +0800

    Installed Mongodb, Wrote out the codes to handle POST request to mongodb Atlas in the Handler function in post-moviejs, Uninstalled FIREBASE and Deleted the Firebase config here, Created A Third Branch FIREBASE BRANCH

--- pages/moviez/api/post-movie.js
+++ pages/moviez/api/post-movie.js
@@ -1,44 +1,45 @@
-import { addDoc,collection,Timestamp } from "firebase/firestore/lite";
-import { firestore } from "../../../firebaseConfig";
+import { MongoClient } from "mongodb";
 
-const Handler =async (req, res) => {
-    if (req.method === "POST") { 
-      const title = req.body.title;
-      const genre = req.body.genre;
-      const image = req.body.image;
-      const year = req.body.year;
-      const description = req.body.description;
-  
-      if (
-        !title ||
-        title.trim() === "" ||
-        !genre ||
-        genre.trim() === "" ||
-        !image ||
-        image.trim() === "" ||
-        !year ||
-        year.trim().length === 0 ||
-        !description ||
-        description.trim().length === 0
-      ) {
-        res.status(401).json({ message: "Invalid Inputs" });
-        return;
-      };
-  
-       const db = firestore()
-       const collectionRef = collection(db, "my-next-moviez")
-       const docRef =  await addDoc(collectionRef,{
-        title: title,
-        genre: genre,
-        image: image,
-        year: year,
-        description: description,
-        createdAt:Timestamp.fromDate(new Date()),
-       });
-       console.log('Document written in ID: ',docRef.id);
-       res.status(200).json({message:'Data Sent'})
+const Handler = async (req, res) => {
+  if (req.method === "POST") {
+    const title = req.body.title;
+    const genre = req.body.genre;
+    const image = req.body.image;
+    const year = req.body.year;
+    const description = req.body.description;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://vawkeicodewebz:rOCzIJz4gHV9to1L@cluster0.9yu1rgh.mongodb.net/?retryWrites=true&w=majority&authMechanism=SCRAM-SHA-1"
+    );
+
+    if (
+      !title ||
+      title.trim() === "" ||
+      !genre ||
+      genre.trim() === "" ||
+      !image ||
+      image.trim() === "" ||
+      !year ||
+      year.trim().length === 0 ||
+      !description ||
+      description.trim().length === 0
+    ) {
+      res.status(401).json({ message: "Invalid Inputs" });
+      return;
     }
-  };
-  
-  export default Handler;
-  
+
+    const movies = {
+      title: title,
+      genre: genre,
+      image: image,
+      year: year,
+      description: description,
+    };
+    const db = client.db("my-next-moviez");
+    const document = await db.collection("moviez").insertOne(movies);
+    res.status(200).json({message:"Movie Added to Database",data:document})
+    client.close()
+  }
+};
+
+export default Handler;
