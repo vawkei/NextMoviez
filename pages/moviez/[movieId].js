66d14a9465de69b60a0d6270cd2640db7f5cac61
voke sokoh <vokes9810@gmail.com>
2023-04-14 22:17:07 +0800

    secured the [movieId].js mongodb atlas api end point for a second time on line 54

--- pages/moviez/[movieId].js
+++ pages/moviez/[movieId].js
@@ -51,7 +51,7 @@ import { MongoClient,ObjectId } from "mongodb";
 // ];
 
 export const getStaticPaths = async()=>{
-  const client =await MongoClient.connect("mongodb+srv://vawkeicodewebz:rOCzIJz4gHV9to1L@cluster0.9yu1rgh.mongodb.net/?retryWrites=true&w=majority&authMechanism=SCRAM-SHA-1");
+  const client =await MongoClient.connect(process.env.MONGODB_URL);
 
   const db = client.db('my-next-moviez');
   const document = db.collection("moviez");
